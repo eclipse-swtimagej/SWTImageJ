b82bbf9e9d80603cc50e36e3da7a31e955d23494
Bio7 <marcel.au@web.de>
2026-07-27 09:20:38 +0200

    Updated SWTImageJ to ImageJ version 1.54u8.

--- org.eclipse.swt.imagej/src/ij/ImageJ.java
+++ org.eclipse.swt.imagej/src/ij/ImageJ.java
@@ -148,7 +148,7 @@ public class ImageJ implements ImageObserver, ShellListener, org.eclipse.swt.eve
 	 * string.
 	 */
 	public static final String VERSION = "1.54u";
-	public static final String BUILD = "7";
+	public static final String BUILD = "8";
 	public static org.eclipse.swt.graphics.Color backgroundColor = new org.eclipse.swt.graphics.Color(237, 237, 237);
 	/** SansSerif, 12-point, plain font. */
 	public static final Font SansSerif12 = new Font("SansSerif", Font.PLAIN, 12);
