0f576612ddfe1ce316bffef1458c54efac490e75
Bio7 <marcel.au@web.de>
2026-09-19 09:49:44 +0200

    Fixed a text settings bug.

--- org.eclipse.swt.imagej/src/ij/plugin/Text.java
+++ org.eclipse.swt.imagej/src/ij/plugin/Text.java
@@ -6,6 +6,7 @@ import java.awt.Font;
 import java.awt.GraphicsEnvironment;
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.eclipse.swt.events.TypedEvent;
 import org.eclipse.swt.widgets.Display;
@@ -26,9 +27,10 @@ import ij.gui.Toolbar;
  * displayed when you double click on the text tool.
  */
 public class Text implements PlugIn, DialogListener {
+
 	private static final String LOC_KEY = "fonts.loc";
-	private static final String[] styles = { "Plain", "Bold", "Italic", "Bold+Italic" };
-	private static final String[] justifications = { "Left", "Center", "Right" };
+	private static final String[] styles = {"Plain", "Bold", "Italic", "Bold+Italic"};
+	private static final String[] justifications = {"Left", "Center", "Right"};
 	private static GenericDialog gd;
 	private String font = TextRoi.getDefaultFontName();
 	private int fontSize = TextRoi.getDefaultFontSize();
@@ -40,32 +42,39 @@ public class Text implements PlugIn, DialogListener {
 	private String colorName;
 
 	public synchronized void run(String arg) {
+
+		AtomicBoolean isVisible = new AtomicBoolean();
 		Display dis = Display.getDefault();
 		dis.syncExec(new Runnable() {
+
 			public void run() {
-				if (gd != null && gd.getShell().isVisible())
-					gd.setActive();
-				else
-					showDialog();
+
+				if(gd != null)
+					isVisible.set(gd.getShell().isVisible());
 			}
 		});
+		if(isVisible.get())
+			gd.setActive();
+		else
+			showDialog();
 	}
 
 	private void showDialog() {
+
 		ImagePlus imp = WindowManager.getCurrentImage();
 		Roi roi = imp != null ? imp.getRoi() : null;
-		TextRoi textRoi = roi != null && (roi instanceof TextRoi) ? (TextRoi) roi : null;
+		TextRoi textRoi = roi != null && (roi instanceof TextRoi) ? (TextRoi)roi : null;
 		String fillc = "None";
 		TextRoi.setDefaultFillColor(null);
 		TextRoi.setDefaultAngle(0.0);
-		if (textRoi != null) {
+		if(textRoi != null) {
 			Font font = textRoi.getCurrentFont();
 			fontSize = font.getSize();
-			angle = (int) textRoi.getAngle();
+			angle = (int)textRoi.getAngle();
 			style = font.getStyle();
 			justification = textRoi.getJustification();
 			Color c = textRoi.getStrokeColor();
-			if (c != null)
+			if(c != null)
 				color = c;
 			fillc = Colors.colorToString2(textRoi.getFillColor());
 			antialiased = textRoi.getAntiAlias();
@@ -81,11 +90,11 @@ public class Text implements PlugIn, DialogListener {
 		gd.addSlider("Angle:", -90, 90, angle);
 		gd.addCheckbox("Antialiased text", antialiased);
 		Point loc = Prefs.getLocation(LOC_KEY);
-		if (IJ.debugMode) {
+		if(IJ.debugMode) {
 			Dimension screen = IJ.getScreenSize();
 			IJ.log("Fonts: " + loc + " " + screen);
 		}
-		if (loc != null) {
+		if(loc != null) {
 			gd.centerDialog(false);
 			gd.setLocation(new org.eclipse.swt.graphics.Point(loc.x, loc.y));
 		}
@@ -95,7 +104,9 @@ public class Text implements PlugIn, DialogListener {
 		gd.showDialog();
 		Display dis = Display.getDefault();
 		dis.syncExec(new Runnable() {
+
 			public void run() {
+
 				org.eclipse.swt.graphics.Point location = gd.getLocation();
 				Prefs.saveLocation(LOC_KEY, new Point(location.x, location.y));
 			}
@@ -103,33 +114,35 @@ public class Text implements PlugIn, DialogListener {
 	}
 
 	String[] getFonts() {
+
 		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
 		String[] fonts = ge.getAvailableFontFamilyNames();
 		ArrayList names = new ArrayList();
 		names.add("SansSerif");
 		names.add("Serif");
 		names.add("Monospaced");
-		for (int i = 0; i < fonts.length; i++) {
+		for(int i = 0; i < fonts.length; i++) {
 			String f = fonts[i];
-			if (f.length() <= 20 && !(f.equals("SansSerif") || f.equals("Serif") || f.equals("Monospaced")))
+			if(f.length() <= 20 && !(f.equals("SansSerif") || f.equals("Serif") || f.equals("Monospaced")))
 				names.add(f);
 		}
-		return (String[]) names.toArray(new String[names.size()]);
+		return (String[])names.toArray(new String[names.size()]);
 	}
 
 	public boolean dialogItemChanged(GenericDialog gd, TypedEvent e) {
+
 		ImagePlus imp = WindowManager.getCurrentImage();
 		Roi roi = imp != null ? imp.getRoi() : null;
-		TextRoi textRoi = roi != null && (roi instanceof TextRoi) ? (TextRoi) roi : null;
+		TextRoi textRoi = roi != null && (roi instanceof TextRoi) ? (TextRoi)roi : null;
 		font = gd.getNextChoice();
 		style = gd.getNextChoiceIndex();
 		justification = gd.getNextChoiceIndex();
 		String colorName2 = gd.getNextChoice();
 		String fillc = gd.getNextChoice();
-		fontSize = (int) gd.getNextNumber();
-		angle = (int) gd.getNextNumber();
+		fontSize = (int)gd.getNextNumber();
+		angle = (int)gd.getNextNumber();
 		antialiased = gd.getNextBoolean();
-		if (colorName != null && !colorName2.equals(colorName)) {
+		if(colorName != null && !colorName2.equals(colorName)) {
 			Color color = Colors.decode(colorName2, null);
 			Toolbar.setForegroundColor(color);
 			colorName = colorName2;
@@ -139,7 +152,7 @@ public class Text implements PlugIn, DialogListener {
 		Color fillColor = Colors.decode(fillc, null);
 		TextRoi.setDefaultFillColor(fillColor);
 		TextRoi.setDefaultAngle(angle);
-		if (textRoi != null) {
+		if(textRoi != null) {
 			textRoi.setAngle(angle);
 			textRoi.setJustification(justification);
 			textRoi.setFillColor(fillColor);
@@ -147,5 +160,4 @@ public class Text implements PlugIn, DialogListener {
 		}
 		return true;
 	}
-
 }
